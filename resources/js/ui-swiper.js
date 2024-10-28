10fed3628b46fae3bcd80cd33e0af4e51c60c69d
seoyoung0929 <csyoung0929@gmail.com>
2024-10-28 09:33:31 +0900

    sec-card 레이아웃 구성

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,30 +1,34 @@
-var swiper = new Swiper('.slide-intro', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  // autoplay: true,
-  pagination: {
-    el: '.slide-intro .swiper-pagination',
-    clickable: true,
-  },
-});
+// var swiper = new Swiper('.slide-intro', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   // autoplay: true,
+//   pagination: {
+//     el: '.slide-intro .swiper-pagination',
+//     clickable: true,
+//   },
+// });
 
 var swiper = new Swiper('.slide-studio', {
-  slidesPerView: 1,
+  slidesPerView: 'auto',
+  //slidesPerView: 1,
   spaceBetween: 20,
-  // slidesOffset: 30,
+  //slidesOffsetAfter: 150,
   pagination: {
     el: '.slide-studio .swiper-pagination',
     clickable: true,
   },
   breakpoints: {
     600: {
-      slidesPerView: 2,
+      slidesPerView: 'auto',
+      //slidesPerView: 2,
       spaceBetween: 20,
+      //slidesOffsetAfter: 10,
     },
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 'auto',
       spaceBetween: 20,
+      slidesOffsetAfter: 30,
     },
   },
 });
